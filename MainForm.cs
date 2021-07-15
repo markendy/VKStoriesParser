cf714eeb5222eab612fd8431e072831e75fcd11e
markendy <alex-repou@yandex.ru>
2021-07-16 02:21:42 +0300

    edit regex

--- MainForm.cs
+++ MainForm.cs
@@ -8,14 +8,12 @@ namespace VKStoriesParser
     public partial class MainForm : Form
     {
         private Regex _globalPattern;
-        private Regex _srcPattern;
 
         public MainForm()
         {
             InitializeComponent();
 
-            _globalPattern = new Regex("class=\"stories_video\".*src=\"https://.*;");
-            _srcPattern = new Regex("(?<=src=\").*(?=\"\\sstyle)");
+            _globalPattern = new Regex("(?<=class=\"stories_video\".*)https://.*?\"");
         }
 
         private string GetUrl(Regex pattern, string text)
@@ -23,20 +21,20 @@ namespace VKStoriesParser
             string result = String.Empty;
 
             MatchCollection matchCollection = pattern.Matches(text);
-
+            
             foreach (var match in matchCollection)
             {
                 result += match.ToString();
             }
 
-            return result;
+            return result.Replace("\"", "");
         }
 
         private void InputTextBox_TextChanged(object sender, EventArgs e)
         {
             try
             {
-                OutputBox.Text = GetUrl(_srcPattern, GetUrl(_globalPattern, InputTextBox.Text));
+                OutputBox.Text = GetUrl(_globalPattern, InputTextBox.Text);
             }
             catch (Exception ex)
             {
