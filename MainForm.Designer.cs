74f3a9ac25d746b5e6781c648bd6c5ffcafd7bfd
markendy <alex-repou@yandex.ru>
2021-07-16 01:36:53 +0300

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace VKStoriesParser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextBox.MaxLength = 0;
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputTextBox.Size = new System.Drawing.Size(504, 100);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(12, 125);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputBox.Size = new System.Drawing.Size(504, 100);
+            this.OutputBox.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 234);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.InputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "VKStoriesParser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace VKStoriesParser
+{
+    public partial class MainForm : Form
+    {
+        private Regex _globalPattern;
+        private Regex _srcPattern;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _globalPattern = new Regex("class=\"stories_video\".*src=\"https://.*;");
+            _srcPattern = new Regex("(?<=src=\").*(?=\"\\sstyle)");
+        }
+
+        private string GetUrl(Regex pattern, string text)
+        {
+            string result = String.Empty;
+
+            MatchCollection matchCollection = pattern.Matches(text);
+
+            foreach (var match in matchCollection)
+            {
+                result += match.ToString();
+            }
+
+            return result;
+        }
+
+        private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                OutputBox.Text = GetUrl(_srcPattern, GetUrl(_globalPattern, InputTextBox.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+    }
+}
